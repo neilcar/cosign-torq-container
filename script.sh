84d8655af4cea9d39e047e3d0b49dc8c3581fa62
neilcar <me@neilcar.com>
2022-11-10 14:08:21 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo $COSIGN_KEY > cosign.key
-cat cosign.key
+echo $COSIGN_KEY > /cosign.key
+cat /cosign.key
 cosign login ghcr.io -u $GHCR_USER -p $GHCR_PASS
-cosign sign -a ORCA_SCAN_FAILED=$SCAN_FAILED --key cosign.key $IMAGE
+cosign sign -a ORCA_SCAN_FAILED=$SCAN_FAILED --key /cosign.key $IMAGE
